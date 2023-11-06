6da059e383546ddd411c51742228ecceb44fc11e
EC2 Default User <ec2-user@ip-172-31-35-70.ap-northeast-1.compute.internal>
2023-11-06 09:09:51 +0000

    basic_arithmetic.phpを書いた

---
+++ basic_arithmetic.php
@@ -0,0 +1,34 @@
+<?php
+
+$i = random_int(1, 999);
+$j = random_int(1, 999);
+
+$k = $i + $j;
+
+echo "{$i} + {$j} = {$k} \n";
+
+$k = $i - $j;
+
+echo "{$i} - {$j} = {$k} \n";
+
+$k = $i * $j;
+
+echo "{$i} * {$j} = {$k} \n";
+
+$k = $i / $j;
+
+echo "{$i} / {$j} = {$k} \n";
+
+$k = $i % $j;
+
+echo "{$i} % {$j} = {$k} \n";
+
+$k = $i ** 3;
+
+echo "{$i} ** {$j} = {$k} \n";
+
+$k = pow($i, 3);
+
+echo "pow({$i}, 3) = {$k} \n";
+
+
