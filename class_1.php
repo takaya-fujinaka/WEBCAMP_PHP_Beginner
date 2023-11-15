f940d5b3a00fe4f13ee563877c06fbb13c67575d
EC2 Default User <ec2-user@ip-172-31-35-70.ap-northeast-1.compute.internal>
2023-11-15 06:21:58 +0000

    class_1.phpを書いた

---
+++ class_1.php
@@ -0,0 +1,23 @@
+<?php
+class クラス名
+{
+    private $変数;
+    public function 関数()
+    {
+        echo "メソッドの処理 \n";
+    }
+    public function set変数($v)
+    {
+        $this->変数 = $v;
+    }
+    public function get変数()
+    {
+        return $this->変数;
+    }
+}
+$obj = new クラス名();
+var_dump($obj);
+$obj->関数();
+$obj->set変数('data');
+echo $obj->get変数() , "\n";
+
