ee43f0b01eb38a70df93a8575ade1a6db0c44dd1
EC2 Default User <ec2-user@ip-172-31-35-70.ap-northeast-1.compute.internal>
2023-11-15 09:19:29 +0000

    trait.phpを書いた

---
+++ trait.php
@@ -0,0 +1,19 @@
+<?php
+trait 部品
+{
+    public function トレイトのメソッド()
+    {
+        echo "トレイトのメソッド \n";
+    }
+}
+class クラス名
+{
+    use 部品;
+    public function クラスのメソッド()
+    {
+        echo "クラスのメソッド \n";
+    }
+}
+$obj = new クラス名();
+$obj->クラスのメソッド();
+$obj->トレイトのメソッド();
